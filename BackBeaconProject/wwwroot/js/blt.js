51f280d349844ddb75ee4e6eea423e94c20a426a
smallwesleyTE <wesley.small@traveledge.com>
2019-05-22 17:25:37 -0400

    Initial Javascript prototype

---
+++ BackBeaconProject/wwwroot/js/blt.js
@@ -0,0 +1,41 @@
+﻿
+
+
+BLT.event = function () {
+    this.campaign = '';
+    this.action = '';
+    this.attributes = {};
+    this.pagetoken = '';    
+    this.setAttribute('timestamp', OWA.util.getCurrentUnixTimestamp());
+}
+
+
+BLT.event.prototype = {
+    get: function (name) {
+        if (this.attributes.hasOwnProperty(name)) {
+            return this.attributes[name];
+        }
+    },
+    setAttribute: function (keyname, attrvalue) {
+        this.attributes[keyname] = attrvalue;
+    },
+    setEventType: function (event_type) {
+        this.set("event_type", event_type);
+    },
+    getAttributes: function () {
+        return this.attributes;
+    },
+    merge: function (attribs) {
+        for (keyname in attribs) {
+            if (attribs.hasOwnProperty(keyname)) {
+                this.set(param, attribs[keyname]);
+            }
+        }
+    },
+    isSet: function (name) {
+        if (this.attributes.hasOwnProperty(name)) {
+            return true;
+        }
+    }
+}
+
