00fa963a1c0afb9bf13e94f2c81d480593dfa583
Wesley Small <smallwesley@gmail.com>
2019-05-23 08:11:15 -0400

    dev in progress

--- BackBeaconProject/wwwroot/js/blt.js
+++ BackBeaconProject/wwwroot/js/blt.js
@@ -39,3 +39,99 @@ BLT.event.prototype = {
     }
 }
 
+OWA.commandQueue = function () {
+    OWA.debug('Command Queue object created');
+    var asyncCmds = [];
+    var is_paused = false;
+}
+
+OWA.commandQueue.prototype = {
+    push: function (cmd, callback) {
+
+        //alert(func[0]);
+        var args = Array.prototype.slice.call(cmd, 1);
+        //alert(args);
+
+        var obj_name = '';
+        var method = '';
+        var check = OWA.util.strpos(cmd[0], '.');
+
+        if (!check) {
+            obj_name = 'OWATracker';
+            method = cmd[0];
+        } else {
+            var parts = cmd[0].split('.');
+            obj_name = parts[0];
+            method = parts[1];
+        }
+
+        OWA.debug('cmd queue object name %s', obj_name);
+        OWA.debug('cmd queue object method name %s', method);
+
+        if (method === "pause-   ") {
+
+            this.pause();
+        }
+
+        // check to see if the command queue has been paused
+        // used to stop tracking		
+        if (!this.is_paused) {
+
+            // is OWATracker created?
+            if (typeof window[obj_name] == "undefined") {
+                OWA.debug('making global object named: %s', obj_name);
+                window[obj_name] = new OWA.tracker({ globalObjectName: obj_name });
+            }
+
+            window[obj_name][method].apply(window[obj_name], args);
+        }
+
+        if (method === "unpause-owa") {
+
+            this.unpause();
+        }
+
+        if (callback && (typeof callback == 'function')) {
+            callback();
+        }
+
+    },
+
+    loadCmds: function (cmds) {
+        this.asyncCmds = cmds;
+    },
+
+    process: function () {
+
+        var that = this;
+        var callback = function () {
+            // when the handler says it's finished (i.e. runs the callback)
+            // We check for more tasks in the queue and if there are any we run again
+            if (that.asyncCmds.length > 0) {
+                that.process();
+            }
+        }
+
+        // give the first item in the queue & the callback to the handler
+        this.push(this.asyncCmds.shift(), callback);
+
+
+		/*
+		for (var i=0; i < this.asyncCmds.length;i++) {
+			this.push(this.asyncCmds[i]);
+		}
+		*/
+    },
+
+    pause: function () {
+
+        this.is_paused = true;
+        OWA.debug('Pausing Command Queue');
+    },
+
+    unpause: function () {
+
+        this.is_paused = false;
+        OWA.debug('Un-pausing Command Queue');
+    }
+};
